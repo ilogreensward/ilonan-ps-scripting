4d8bea3b6d880afc2893814fb28edae8d63ac8ab
Ilona Nurminen <ilogreensward@gmail.com>
2017-07-10 15:51:48 +0300

    First commit

---
+++ AddCurrentUserToLocalAdmin.ps1
@@ -0,0 +1,14 @@
+﻿#Add currently logged in user to local administrator group
+#ilogreensward 2017
+
+$domain = (Get-WmiObject Win32_ComputerSystem).Domain
+echo "User currently logged in: $env:username"
+echo "Domain: $domain"
+$confirmation = Read-Host "Do you want to make $env:username into a local administrator on this computer? Y/N"
+
+IF($confirmation -eq 'y'){
+net localgroup Administrators "$domain\$env:username" /ADD
+}
+IF($confirmation -eq 'n'){
+echo "Operation cancelled."
+}
